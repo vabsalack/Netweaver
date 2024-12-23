ce18d4ef3a4329b007d9c4b75f004bdb60d6a15a
kittu <keerthi.pydev@gmail.com>
2024-12-23 20:49:36 +0530

    M cneural add optimizer class with LR and decay

--- nn-package/cneural.py
+++ nn-package/cneural.py
@@ -5,20 +5,23 @@ class Layer_Dense:
 
     # Layer initialization
     def __init__(self, n_inputs, n_neurons):
-        # Initialize weights and biases
-        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons) # s:[[]]
-        self.biases = np.zeros((1, n_neurons)) # s: [[]]
+        # the weight's initialization significantly impacts convergence
+        # (T) he initialization. variance scaled by number of input connections. σ2 = 2 / n_in
+        # below weights matrix initialization, notice dim is ninputs x nneurons, make easier for mat mul. 
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons) 
+        self.biases = np.zeros((1, n_neurons))
 
     # Forward pass
     def forward(self, inputs):
-        # Remember input values
         self.inputs = inputs
         # Calculate output values from inputs, weights and biases
+        # In output matrix, rows constitues each sample and column constitues each neurons in a layer.
         self.output = np.dot(inputs, self.weights) + self.biases
         
     # Backward pass
     def backward(self, dvalues):
         # Gradients on parameters
+        # dvalues are backpropogated gradient from next layer
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
         # Gradient on values
@@ -30,7 +33,6 @@ class Activation_ReLU:
 
     # Forward pass
     def forward(self, inputs):
-        # Remember input values
         self.inputs = inputs
         # Calculate output values from inputs
         self.output = np.maximum(0, inputs)
@@ -49,19 +51,22 @@ class Activation_Softmax:
 
     # Forward pass
     def forward(self, inputs):
-        # Remember input values
         self.inputs = inputs
         # Get unnormalized probabilities
+        # exponentiation graph is rapid, avoid overflow by making maximum to zero. get the idea
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         # Normalize them for each sample
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
+
         # Backward pass
+        # Backward pass
+
+    # Backward pass
 
     def backward(self, dvalues):
         # Create uninitialized array
         self.dinputs = np.empty_like(dvalues)
-        # Enumerate outputs and gradients
         for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
             # Flatten output array
             single_output = single_output.reshape(-1, 1)
@@ -75,13 +80,9 @@ class Activation_Softmax:
 # Common loss class
 class Loss:
     # Calculates the data and regularization losses
-    # given model output and ground truth values
     def calculate(self, output, y):
-        # Calculate sample losses
         sample_losses = self.forward(output, y)
-        # Calculate mean loss
         data_loss = np.mean(sample_losses)
-        # Return loss
         return data_loss
     
 # Cross-entropy loss
@@ -97,15 +98,15 @@ class Loss_CategoricalCrossentropy(Loss):
         # only if categorical labels
         if len(y_true.shape) == 1:
             correct_confidences = y_pred_clipped[range(samples), y_true]
-        
         elif len(y_true.shape) == 2:
             correct_confidences = np.sum(y_pred_clipped * y_true,axis=1)
-            # Losses
-            negative_log_likelihoods = -np.log(correct_confidences)
-            return negative_log_likelihoods
+        # Losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
         
     # Backward pass
     def backward(self, dvalues, y_true):
+        # davalues are softmax outputs (predicted outputs)
         # Number of samples
         samples = len(dvalues)
         # Number of labels in every sample
@@ -126,8 +127,8 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
     def __init__(self):
         self.activation = Activation_Softmax()
         self.loss = Loss_CategoricalCrossentropy()
-        # Forward pass
 
+    # Forward pass
     def forward(self, inputs, y_true):
         # Output layer's activation function
         self.activation.forward(inputs)
@@ -146,10 +147,38 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
             y_true = np.argmax(y_true, axis=1)
         # Copy so we can safely modify
         self.dinputs = dvalues.copy()
-        # Calculate gradient
+        # Calculate gradient, true lables are 1
         self.dinputs[range(samples), y_true] -= 1
         # Normalize gradient
         self.dinputs = self.dinputs / samples
+
+
+class Optimizer_SGD:
+    # learning rate of 1. is default for this optimizer
+    def __init__(self, learning_rate=1., decay=0.):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+
+    # call once before any parameter updates
+    def pre_update_params(self):
+        if self.decay:
+            # adding 1 ensure value decreases
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+
+    # update parameters
+    def update_params(self, Layer):
+        Layer.weights += -self.learning_rate * Layer.dweights
+        Layer.biases += -self.learning_rate * Layer.dbiases
+
+    # call once after any parameter updates
+    def post_update_params(self):
+        self.iterations += 1
+        
+
+
+           
 
 
     
