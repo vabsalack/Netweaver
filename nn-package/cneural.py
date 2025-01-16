03184b0638b91d4d89502e88db768903542f94f3
kittu <keerthi.pydev@gmail.com>
2025-01-16 22:15:48 +0530

    M cneural.py add L1 and L2

--- nn-package/cneural.py
+++ nn-package/cneural.py
@@ -4,12 +4,22 @@ import numpy as np
 class Layer_Dense:
 
     # Layer initialization
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons,
+                 weight_regularizer_L1=0, weight_regularizer_L2=0,
+                 bias_regularizer_L1=0, bias_regularizer_L2=0):
         # the weight's initialization significantly impacts convergence
         # (T) he initialization. variance scaled by number of input connections. σ2 = 2 / n_in
         # below weights matrix initialization, notice dim is ninputs x nneurons, make easier for mat mul. 
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons) 
         self.biases = np.zeros((1, n_neurons))
+
+        # set regularization strength
+        # weight regu..
+        self.weight_regularizer_L1 = weight_regularizer_L1
+        self.weight_regularizer_L2 = weight_regularizer_L2
+        # bias regu..
+        self.bias_regularizer_L1 = bias_regularizer_L1
+        self.bias_regularizer_L2 = bias_regularizer_L2
 
     # Forward pass
     def forward(self, inputs):
@@ -24,6 +34,26 @@ class Layer_Dense:
         # dvalues are backpropogated gradient from next layer
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradients on regularization
+        # L1 on weights
+        if self.weight_regularizer_L1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_L1 * dL1
+        # L2 on weights
+        if self.weight_regularizer_L2 > 0:
+            self.dweights += 2 * self.weight_regularizer_L2 * self.weights
+        
+        # L1 on biases
+        if self.bias_regularizer_L1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_L1 * dL1
+        # L2 on biases
+        if self.bias_regularizer_L2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_L2 * self.biases
+
         # Gradient on values
         self.dinputs = np.dot(dvalues, self.weights.T)
 
@@ -79,10 +109,32 @@ class Activation_Softmax:
 
 # Common loss class
 class Loss:
+    # Regularization loss calculation
+    def regularization_loss(self,layer):
+        
+        # 0 by default
+        regularization_loss = 0
+
+        # L1 and L2 penalties for weights
+        if layer.weight_regularizer_L1 > 0:
+            regularization_loss += layer.weight_regularizer_L1 * np.sum(np.abs(layer.weights))
+        if layer.weight_regularizer_L2 > 0:
+            regularization_loss += layer.weight_regularizer_L2 * np.sum(layer.weights * layer.weights)
+        # L1 and L2 penalties for biases
+        if layer.bias_regularizer_L1 > 0:
+            regularization_loss += layer.bias_regularizer_L1 * np.sum(np.abs(layer.biases))
+        if layer.bias_regularizer_L2 > 0:
+            regularization_loss += layer.bias_regularizer_L2 * np.sum(layer.biases * layer.biases)
+
+        return regularization_loss
+
     # Calculates the data and regularization losses
     def calculate(self, output, y):
+        # sample losses
         sample_losses = self.forward(output, y)
+        # mean loss
         data_loss = np.mean(sample_losses)
+        
         return data_loss
     
 # Cross-entropy loss
